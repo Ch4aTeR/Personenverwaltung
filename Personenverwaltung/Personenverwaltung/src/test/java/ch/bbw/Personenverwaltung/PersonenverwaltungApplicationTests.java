fe1a6c1dec0ecf8d7ff5956261dcb819e014680d
Ch4aTeR <47777728+Ch4aTeR@users.noreply.github.com>
2022-06-27 18:13:55 +0200

    Add files via upload

---
+++ Personenverwaltung/Personenverwaltung/src/test/java/ch/bbw/Personenverwaltung/PersonenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.Personenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonenverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
